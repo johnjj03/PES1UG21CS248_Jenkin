59a4a69597c141009a8f7a6fae1af6e61fa7537f
John J J <83080938+johnjj03@users.noreply.github.com>
2024-03-06 21:11:36 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "New change made for task 4\n';
+  cout << "New change made for task 4\n";
   return 0;
 }
